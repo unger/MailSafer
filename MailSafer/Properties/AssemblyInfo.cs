ce5aabefa448f550e833e17b018f31329f0c8b7e
Magnus Unger <magnus_unger@hotmail.com>
2015-03-26 23:53:19 +0100

    Updated description in AssemblyInfo

--- MailSafer/Properties/AssemblyInfo.cs
+++ MailSafer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MailSafer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SmtpClient wrapper to simplify monitoring of sending emails")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MailSafer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Magnus Unger 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
