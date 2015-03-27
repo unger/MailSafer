3a46111e973482c1855c2a102966ad825c37079c
Magnus Unger <magnus_unger@hotmail.com>
2015-03-27 08:53:25 +0100

    Added log4net logger with seperate NuGet package

---
+++ MailSafer.Logger.log4net/Log4NetMailLogger.cs
@@ -0,0 +1,22 @@
+﻿namespace MailSafer.Logger
+{
+    using System;
+    using System.Net.Mail;
+
+    using log4net;
+
+    public class Log4NetMailLogger : IMailLogger
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Log4NetMailLogger));
+
+        public void Info(string message, MailMessage mailMessage)
+        {
+            Log.InfoFormat("{0} {1} {2} {3}", message, mailMessage.From, mailMessage.To, mailMessage.Subject);
+        }
+
+        public void Error(string message, MailMessage mailMessage, Exception exception)
+        {
+            Log.Error(string.Format("{0} {1} {2} {3}", message, mailMessage.From, mailMessage.To, mailMessage.Subject), exception);
+        }
+    }
+}
